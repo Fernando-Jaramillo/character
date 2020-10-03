9a043ef837a44446bce7c274eab9f3cfa0a5e5f0
Fernando <lenin_jaramillo@hotmail.com>
2020-10-02 19:54:38 -0500

    pagination buttons

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,22 @@
     </header>
     <div class="container">
       <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
-        <Character v-for="character in characters" v-bind:key="character.id" v-bind:character="character"/>
+        <Character 
+        v-for="character in characters" 
+        v-bind:key="character.id" 
+        v-bind:character="character"/>
       </div>
+      <nav class="pagination" role="navegation" aria-label="pagination">
+        <a class="pagination-previous">Before</a>  
+        <ul class="pagination-list">
+          <li>
+            <a class="pagination-link is-current">{{ page }}</a>
+          </li>
+        </ul>
+
+        <a class="pagination-next">Next</a>      
+
+      </nav>
     </div>  
   </div>
 </template>
@@ -29,7 +43,9 @@ export default {
   },
   data() {
     return {
-      characters: []
+      characters: [],
+      page: 1,
+      pages: 1
     }
   },
   created(){
@@ -42,6 +58,7 @@ export default {
         .then((res) => {
           console.log(res.data);
           it.characters = res.data.results;
+          it.pages = res.data.info.pages
           })
         .catch((err) => {
           console.log(err)
